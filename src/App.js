99ac48ee989150578ad6b00bd7145c5cf4f8d922
siddhant <siddhantp736@gmail.com>
2022-03-13 15:45:50 +0530

    Created feature-2 branch

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <h1>Hello let's build a cart</h1>
       <h2>This is from the new "MyFeature Branch"</h2>
+      <h2>This is from the new "feature-2 Branch"</h2>
     </div>
   );
 }
