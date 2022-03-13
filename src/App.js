27e7072724c7c568e6de02c13e6e9b4b84780ff9
siddhant <siddhantp736@gmail.com>
2022-03-13 15:01:44 +0530

    bug fixes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>This is the react demo to netlify</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Let's build a cart here using react hooks
-        </a>
-      </header>
+      <h1>Hello let's build a cart</h1>
     </div>
   );
 }
