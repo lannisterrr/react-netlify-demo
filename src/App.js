53a903b49f2b1a5aef4e85d3c3221fc9d6f1f8db
siddhant <siddhantp736@gmail.com>
2022-03-13 14:41:39 +0530

    added text

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Let's build a cart here using react hooks
         </a>
       </header>
     </div>
